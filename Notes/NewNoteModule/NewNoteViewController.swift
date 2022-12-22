200549f6274a1aa8a79516ccc57ac382e172b747
Pavelweb22 <pavelweb2016@yandex.ru>
2022-12-22 21:05:12 +0300

    realm

--- Notes/NewNoteModule/NewNoteViewController.swift
+++ Notes/NewNoteModule/NewNoteViewController.swift
@@ -19,7 +19,26 @@ class NewNoteViewController: UIViewController {
         return button
     }()
     
-    private let titleNoteLabel = UILabel(text: "Название", font: .robotoMedium14(), textColor: .specialLightBrown)
+    private let titleNoteLabel = UILabel(text: "Название заметки", font: .robotoMedium14(), textColor: .specialLightBrown)
+    
+    private let titleNoteTextField = BrownTextField()
+    
+    private let detailsNoteLabel = UILabel(text: "Подробно", font: .robotoMedium14(), textColor: .specialLightBrown)
+    
+    private let detailsNoteTextField = BrownTextField()
+    
+    
+    private let saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Сохранить", for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .specialGreen
+        button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +52,19 @@ class NewNoteViewController: UIViewController {
         view.addSubview(titleLabel)
         view.addSubview(closeButton)
         view.addSubview(titleNoteLabel)
+        view.addSubview(titleNoteTextField)
+        view.addSubview(detailsNoteLabel)
+        view.addSubview(detailsNoteTextField)
+        view.addSubview(saveButton)
     }
 
     @objc private func closeButtonTapped() {
         dismiss(animated: true)
         print("closeButtonTapped")
+    }
+    
+    @objc private func saveButtonTapped() {
+        print("saveButtonTapped")
     }
     
 }
@@ -57,7 +84,25 @@ extension NewNoteViewController {
             closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -25),
             
             titleNoteLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
-            titleNoteLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25)
+            titleNoteLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25),
+            
+            titleNoteTextField.topAnchor.constraint(equalTo: titleNoteLabel.bottomAnchor, constant: 3),
+            titleNoteTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 14),
+            titleNoteTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14),
+            titleNoteTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            detailsNoteLabel.topAnchor.constraint(equalTo: titleNoteTextField.bottomAnchor, constant: 16),
+            detailsNoteLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25),
+            
+            detailsNoteTextField.topAnchor.constraint(equalTo: detailsNoteLabel.bottomAnchor, constant: 3),
+            detailsNoteTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 14),
+            detailsNoteTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14),
+            detailsNoteTextField.heightAnchor.constraint(equalToConstant: 100),
+            
+            saveButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -120),
+            saveButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            saveButton.heightAnchor.constraint(equalToConstant: 55),
+            saveButton.widthAnchor.constraint(equalToConstant: 300)
         
         ])
     }
